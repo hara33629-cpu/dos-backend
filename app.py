61d8a1ab98d7cbf6e8b9e3d9466f277aed3a2efc
Haritha <hara33629@gmail.com>
2026-04-08 14:02:56 +0530

    Refactor email alert handling to use utility function
    Refactored email alert functionality by importing send_email_alert from utils and removing hardcoded email logic.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 
+from utils.send_email_alert import send_email_alert
 from utils.predictor import predict_trust_score
 
 # =========================
@@ -103,42 +104,6 @@ def create_alert(ip, message):
         conn.close()
     except Exception as e:
         print("❌ Alert Error:", e)
-
-def send_email_alert(ip, decision, threat_score, features):
-    try:
-        # ✅ HARD CODE (PUT YOUR REAL EMAIL HERE)
-        sender = "hara33629@gmail.com"
-        password = "smvzvsikecvqsmvu"
-        receiver = "haritha022006@gmail.com"
-
-        subject = f"🚨 DOS Alert - {decision}"
-
-        html_body = f"""
-        <h2>🚨 DOS Alert</h2>
-        <p><b>IP:</b> {ip}</p>
-        <p><b>Decision:</b> {decision}</p>
-        <p><b>Threat Score:</b> {threat_score}</p>
-        """
-
-        msg = MIMEText(html_body, "html")
-        msg["Subject"] = subject
-        msg["From"] = sender
-        msg["To"] = receiver
-
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-
-        print("🔐 Logging into email...")
-        server.login(sender, password)
-
-        print("📤 Sending email...")
-        server.sendmail(sender, receiver, msg.as_string())
-
-        server.quit()
-        print("✅ Email sent successfully!")
-
-    except Exception as e:
-        print("❌ Email Error:", str(e))
 
 # =========================
 # IP BLACKLIST
